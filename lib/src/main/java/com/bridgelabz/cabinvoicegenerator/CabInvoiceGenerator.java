eb886f8d95ce2c56652dda452c429b8879d27cba
TheJayesh99 <jayeshm.official@gmail.com>
2021-07-25 17:46:28 +0530

    Added : Initials files required for programs

---
+++ lib/src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenerator
+{
+
+}
---
+++ lib/src/test/java/com/bridgelabz/cabinvoicegeneratorTest/CabInvoiceGeneratorTest.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.cabinvoicegeneratorTest;
+
+public class CabInvoiceGeneratorTest 
+{
+
+}
